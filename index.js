9b9357f3bc0392dbe9fddf6076809357414ae135
Ashutosh Chaudhary <216.ashutosh@gmail.com>
2020-10-02 13:53:25 +0530

    Changed body to panelBody

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const root = document.createElement('div');
-
-ReactDOM.render(
-    <h1>Tab content here!</h1>,
-    root,
-);
-
 export const requestTabs = [
     {
         label: "Example tab",
         icon: "fa-star",
-        body: root,
+        panelBody: (context, { request }) => {
+            const root = document.createElement('div');
+
+            ReactDOM.render(
+                <h1>Tab content here!</h1>,
+                root,
+            );
+
+            return root
+        },
     }
 ];
